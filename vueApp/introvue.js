543496ab9dd2dba51815c8497f4e6caad9daf1d0
Gord Nuttall <gord.nuttall@adswerve.com>
2021-01-26 16:59:16 -0700

    Vue Mastery #6 completed

--- vueApp/introvue.js
+++ vueApp/introvue.js
@@ -2,11 +2,13 @@
       el: '#app',
       data: {
         product: 'Socks',
+        inStock: true,
+        emptyCart: true,
         image: './assets/vmSocks-green-onWhite.jpg',
         addinfolink: 'https://www.google.com',
         imageAltText: 'cool socks',
-        inventory: 100,       //lowercase only
-        goingFast: true,
+        inventory: 10,
+        goingFast: true,      //lowercase only
         onSale: true,
         details: ["80% cotton","20% polyester", "gender-neutral"],
         sizes: ["XL","L", "M", "S", "XS"],
@@ -27,12 +29,20 @@
       methods: {
         addToCart() {
             this.cart += 1
+            this.inventory -= 1
+            this.beforeMount()
         },
         updateProduct(vrntImg) {
             this.image = vrntImg
         },
         removeFromCart() {
             this.cart -= 1
+            this.inventory += 1
+            this.beforeMount()
+        },
+        beforeMount() {
+            this.inStock = this.inventory > 0
+            this.emptyCart = this.cart === 0
         }
 
       }
